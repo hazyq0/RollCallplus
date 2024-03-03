8a065694d37d0b113543986ad9b740bbb0ec5349
ambersub <140431801+ambersub@users.noreply.github.com>
2024-03-03 12:42:59 +0530

    Update MainActivity.java
    vcs check

--- MainActivity.java
+++ MainActivity.java
@@ -108,4 +108,4 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-}
+} f
