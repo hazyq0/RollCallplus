04145589e341274eb4da811902e334b6946d00b2
ambersub <140431801+ambersub@users.noreply.github.com>
2024-03-03 20:45:17 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.rollcall;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -7,19 +10,21 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+import androidx.activity.EdgeToEdge;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.rollcall.databinding.ActivityMainBinding;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -27,113 +32,86 @@ import android.net.Uri;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import android.content.SharedPreferences;
+import com.google.gson.reflect.TypeToken;
 
 
 public class MainActivity extends AppCompatActivity {
-    private String scannedUrl = "";
-    private String name = "";
+    private static final String SHARED_PREFS = "persist data";
+    private static final String SHARED_PREFS_KEY = "list";
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    Button saveButton;
+    EditText textInput;
+    ListView listView;
+    ArrayList<String> items = new ArrayList<>();
+    ArrayAdapter<String> listAdapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        textInput = (EditText) findViewById(R.id.textInput);
+        listView = (ListView) findViewById(R.id.listView);
+        listAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        listView.setAdapter(listAdapter);
+        //load data from sp
+        loadData();
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
 
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-
-        // Change the scanning button to FAB (Floating Action Button)
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Initialize the Intent Integrator
-                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-                integrator.setOrientationLocked(false); // Allow both portrait and landscape orientation
-                integrator.setPrompt("Scan a QR code"); // Set a custom prompt message
-                integrator.setBeepEnabled(false); // Disable beep sound
-                integrator.initiateScan(); // Start QR code scanning
+                String text = textInput.getText().toString();
+                if (text == null || text.trim().equals("")) {
+                    Toast.makeText( MainActivity.this, "Item is empty", Toast.LENGTH_SHORT).
+                            show();
+                    return;
+                }
+                if (items.contains(text)) {
+                    Toast.makeText(MainActivity.this, "Item has already been added", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                items.add(text);
+                listAdapter.notifyDataSetChanged();
+                textInput.setText("");
+
+                //save the data to SP
+                saveData();
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Scan canceled", Toast.LENGTH_SHORT).show();
-            } else {
-                String scannedText = result.getContents();
-                Toast.makeText(this, "Scanned URL: " + scannedUrl, Toast.LENGTH_SHORT).show();
-                // Handle the scanned QR code data (e.g., open a link)
-                try {
-                    name = extractNameFromWebsite(scannedUrl); // Extract website name
-                    System.out.println("Name of the website: " + name);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    private void saveData() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(items);
+        editor.putString(SHARED_PREFS_KEY, jsonString);
+        editor.apply();
 
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loadData() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sp.getString(SHARED_PREFS_KEY, "");
+        if (json.isEmpty()) {
+            return;
         }
-    }
-
-    private String extractNameFromWebsite(String websiteUrl) throws IOException {
-        URL url = new URL(websiteUrl);
-        URLConnection connection = url.openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        String firstLine = reader.readLine(); // Read the first line of the website
-        reader.close();
-
-        // Assuming the name is in the format "name xyz"
-        String[] parts = firstLine.split("\\s+");
-        if (parts.length > 1 && parts[0].equalsIgnoreCase("name")) {
-            return parts[1]; // Return the second part after "name"
-        } else {
-            return "Name not found";
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        Type type = new TypeToken<ArrayList<String>>() {
+        }.getType();
+        items.addAll((gson.fromJson(json, type)));
     }
 }
